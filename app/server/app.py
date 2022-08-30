201696312ee6750e871d8ebd2652e662b9ee4aa0
Kelvin Liao <karevin07@gmail.com>
2022-08-30 17:49:36 +0800

    fix(app.py): unused variable

--- app/server/app.py
+++ app/server/app.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI
 
 from .routes.house import router as Router
 
-db_name = "591"
-collection_name = "item_detail"
-
 app = FastAPI()
 
 app.include_router(Router, tags=["House"], prefix="/house")
